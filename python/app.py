9b372b39d0ec584b13f68b857bc53974ed322780
linuxnomad <23653759+linuxnomad@users.noreply.github.com>
2020-03-01 16:01:37 +0800

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "A civilization that leaves so many of it's citizens unsatisfied and drives them to revolt, neither has nor deserves the prospect of a lasting existance."
+    return "A civilization that leaves so many of it's citizens unsatisfied and drives"
+    return "them to revolt, neither has nor deserves the prospect of a lasting existance."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
